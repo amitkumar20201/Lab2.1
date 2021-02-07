1f7887f2136035bb48360848683fd80bc82f34dc
Ubuntu <amit4kumarcofor@amit4kumarcofor.gr2byh3jsheurfpytq2oyajssd.gx.internal.cloudapp.net>
2021-02-07 15:56:16 +0000

    I am pushing all the files to my GitHub

---
+++ C_Program.c
@@ -0,0 +1,8 @@
+#include<studiio.h>
+ini main(){
+printf("Hello! I am C-Program. Thank you!")
+
+return 0;
+
+}
+
---
+++ HelloJava.java
@@ -0,0 +1,6 @@
+class Hello Java{
+public static void main(String args[]){
+System.out.PrintIn("I am your Java Program. Thank you!");
+} 
+
+}
